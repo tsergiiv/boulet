5ded731203e6453b26e1cc62dc49252ac24805cb
tsergiiv@gmail.com <tsergiiv@gmail.com>
2021-03-05 13:28:51 +0200

    remove icons.css file

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -220,5 +220,6 @@ add_action( 'wp_enqueue_scripts', 'sf_child_theme_dequeue_style', 999 );
  */
 function sf_child_theme_dequeue_style() {
     wp_dequeue_style( 'storefront-style' );
+    wp_dequeue_style( 'storefront-icons' );
     wp_dequeue_style( 'storefront-woocommerce-style' );
 }
