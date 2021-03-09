2d7d345bbfd7146a3ffee27bb74f17997a8e91f8
Рома <skillthorn@gmail.com>
2021-03-09 13:03:10 +0200

    sort dropdown

--- wp-content/themes/storefront-child/woocommerce/loop/orderby.php
+++ wp-content/themes/storefront-child/woocommerce/loop/orderby.php
@@ -20,12 +20,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-<form class="woocommerce-ordering" method="get">
-	<select name="orderby" class="orderby" aria-label="<?php esc_attr_e( 'Shop order', 'woocommerce' ); ?>">
-		<?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
-			<option value="<?php echo esc_attr( $id ); ?>" <?php selected( $orderby, $id ); ?>><?php echo esc_html( $name ); ?></option>
-		<?php endforeach; ?>
-	</select>
-	<input type="hidden" name="paged" value="1" />
-	<?php wc_query_string_form_fields( null, array( 'orderby', 'submit', 'paged', 'product-page' ) ); ?>
+<form class="flex items-baseline sort-form select__dropdown--up" method="get">
+    <label class=" text-ms margin-right-sm" for="select-sorting">Trier par:</label>
+    <div class="select inline-block js-select test" data-trigger-class="reset text-sm text-underline-hover inline-flex items-center cursor-pointer js-tab-focus">
+        <select class="is-hidden" name="sort" id="select-sorting" onchange="this.form.submit()" aria-label="<?php esc_attr_e( 'Shop order', 'woocommerce' ); ?>">
+            <?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
+                <option value="<?php echo esc_attr( $id ); ?>" <?php selected( $orderby, $id ); ?>><?php echo esc_html( $name ); ?></option>
+            <?php endforeach; ?>
+        </select>
+        <input type="hidden" name="paged" value="1" />
+        <?php wc_query_string_form_fields( null, array( 'orderby', 'submit', 'paged', 'product-page' ) ); ?>
+        <svg class="icon icon--xxs margin-left-auto" aria-hidden="true" viewBox="0 0 16 16"><polygon fill="#2C8D86" points="8,11.4 2.6,6 4,4.6 8,8.6 12,4.6 13.4,6 "></polygon></svg>
+    </div>
 </form>
--- wp-content/themes/storefront-child/woocommerce/single-product/tabs/tabs.php
+++ wp-content/themes/storefront-child/woocommerce/single-product/tabs/tabs.php
@@ -30,27 +30,31 @@ $product_tabs = apply_filters( 'woocommerce_product_tabs', array() );
 
 if ( ! empty( $product_tabs ) ) : ?>
 
-	<div class="woocommerce-tabs wc-tabs-wrapper">
-		<ul class="tabs wc-tabs" role="tablist">
-			<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
-				<li class="<?php echo esc_attr( $key ); ?>_tab" id="tab-title-<?php echo esc_attr( $key ); ?>" role="tab" aria-controls="tab-<?php echo esc_attr( $key ); ?>">
-					<a href="#tab-<?php echo esc_attr( $key ); ?>">
-						<?php echo wp_kses_post( apply_filters( 'woocommerce_product_' . $key . '_tab_title', $product_tab['title'], $key ) ); ?>
-					</a>
-				</li>
-			<?php endforeach; ?>
-		</ul>
-		<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
-			<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr( $key ); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>" role="tabpanel" aria-labelledby="tab-title-<?php echo esc_attr( $key ); ?>">
-				<?php
-				if ( isset( $product_tab['callback'] ) ) {
-					call_user_func( $product_tab['callback'], $key, $product_tab );
-				}
-				?>
-			</div>
-		<?php endforeach; ?>
+	<div class="container max-width-lg padding-y-xl">
+        <nav class="tabs js-tabs">
+            <nav class="s-tabs">
+                <ul class="s-tabs__list js-tabs__controls" aria-label="Tabs Interface" role="tablist">
+                    <?php foreach ( $product_tabs as $key => $product_tab ) : ?>
+                        <li role="presentation" class="<?php echo esc_attr( $key ); ?>_tab" id="tab-title-<?php echo esc_attr( $key ); ?>" role="tab" aria-controls="tab-<?php echo esc_attr( $key ); ?>">
+                            <a href="#tab-<?php echo esc_attr( $key ); ?>" class="tabs__control s-tabs__link rating-link_block js-tabs__trigger" role="tab" aria-selected="true" aria-controls="<?php echo esc_attr( $key ); ?>" id="tab-<?php echo esc_attr( $key ); ?>">
+                                <?php echo wp_kses_post( apply_filters( 'woocommerce_product_' . $key . '_tab_title', $product_tab['title'], $key ) ); ?>
+                            </a>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+                <?php foreach ( $product_tabs as $key => $product_tab ) : ?>
+                    <div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr( $key ); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>" role="tabpanel" aria-labelledby="tab-title-<?php echo esc_attr( $key ); ?>">
+                        <?php
+                        if ( isset( $product_tab['callback'] ) ) {
+                            call_user_func( $product_tab['callback'], $key, $product_tab );
+                        }
+                        ?>
+                    </div>
+                <?php endforeach; ?>
 
-		<?php do_action( 'woocommerce_product_after_tabs' ); ?>
+                <?php do_action( 'woocommerce_product_after_tabs' ); ?>
+            </nav>
+        </div>
 	</div>
 
 <?php endif; ?>
